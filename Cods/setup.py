fc33902e38baa78fe2e99ea1211660511e0bd74c
Amir Hossein Karami <a.h.karami110@gmail.com>
2024-09-13 11:06:29 +0000

    update setup.py

--- Cods/setup.py
+++ Cods/setup.py
@@ -1,6 +1,6 @@
 from preprossing.TextEmbbeding import ModelUtils, preprossesor, Preprocess
 
-parsbert_root = "./Cods/models/TextEmbedding"
+parsbert_root = "./Cods/models/TextEmbedding" # This should be changed in the futeure
 
 utils = ModelUtils(parsbert_root)
 utils.download_model()

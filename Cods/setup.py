430b4bb4f60976b1700f9199724ecdc9ba5e4b3e
Amir Hossein Karami <a.h.karami110@gmail.com>
2024-09-13 10:53:03 +0000

    /home/zeus/miniconda3/envs/cloudspace/bin/python /teamspace/studios/this_studio/image-text/Cods/setup.py

--- Cods/setup.py
+++ Cods/setup.py
@@ -1,6 +1,6 @@
 from preprossing.TextEmbbeding import ModelUtils, preprossesor, Preprocess
 
-parsbert_root = "models/TextEmbedding"
+parsbert_root = "./Cods/models/TextEmbedding"
 
 utils = ModelUtils(parsbert_root)
 utils.download_model()

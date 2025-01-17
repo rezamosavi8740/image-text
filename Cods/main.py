463f605858b55e726cf3431e20f66fe47c692ded
rezamosavi <rezamosavi@rezamosavis-MacBook-Air.local>
2025-01-18 03:11:52 +0330

    add edite

--- Cods/main.py
+++ Cods/main.py
@@ -1,9 +1,9 @@
 from Cods.mainPage import WebApp
 from Cods.preprossing.TextEmbbeding import ModelUtils, preprossesor, Preprocess
 
-# parsbert_root = "../Cods/models/TextEmbedding"  # This path should be added to .gitignore
-# utils = ModelUtils(parsbert_root)
-# utils.download_model()
+parsbert_root = "Cods/models/TextEmbedding"  # This path should be added to .gitignore
+utils = ModelUtils(parsbert_root)
+utils.download_model()
 
 web_app = WebApp()
 
